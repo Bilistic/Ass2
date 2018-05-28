f0d85899d12eb967b27b981905aae8e6c8b69719
Bilistic <joshua.nuttall@mycit.ie>
2018-05-28 03:11:27 +0100

    Fixed test file

--- test.py
+++ test.py
@@ -1,22 +1,34 @@
 from Server import Server
+import unittest
 
+class TestFibonacci(unittest.TestCase):
 
-"""
-tests sentiment analysis on server.
-"""
-def test_sentiment():
-    sent_scorer = Server.DataAnalysis()
-    obj = {'length': 80, 'method': 'reddit',
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    """
+    tests cleaning text on server.
+    """
+    def test_twitter(self):
+        sent_scorer = Server.DataAnalysis()
+        sentence = "[Dirty-] sentence that (/needs/) {cleaning}"
+        ans = sent_scorer.clean(sentence)
+        self.assertEqual(ans, "Dirty sentence that needs cleaning")
+        
+        
+    """
+    tests sentiment analysis on server.
+    """
+    def test_sentiment(self):
+        sent_scorer = Server.DataAnalysis()
+        obj = {'length': 80, 'method': 'reddit',
            'text': 'I m A Democrat And The Left s Russia Gaslighting Scares Me More Than Trump Does'}
-    ans = sent_scorer.analyze_sentiment(obj)
-    assert int(ans.sentiment) == 0.25
+        ans = sent_scorer.analyze_sentiment(obj)
+        self.assertEqual(int(ans.sentiment), 0.25)
 
-
-"""
-tests cleaning text on server.
-"""
-def test_twitter():
-    sent_scorer = Server.DataAnalysis()
-    sentence = "[Dirty-] sentence that (/needs/) {cleaning}"
-    ans = sent_scorer.clean(sentence)
-    assert ans == "Dirty sentence that needs cleaning"
+    
+if __name__ == '__main__':
+  unittest.main()
